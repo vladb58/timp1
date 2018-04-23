16a97880d2d7fb36f31db65408a5f15c160e788b
vladb58 <belonogoff.vladislaw@yandex.ru>
2018-04-23 20:37:41 +0300

    Add testGit.cpp

---
+++ testGit.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char **argv, char z)
+{
+	double arg1, arg2, res;
+	char *error;
+	arg1 = strtod(argv[1], &error);
+	if (*error != '\0')
+	{
+		cout << "It's not number, please enter number" << endl;
+		exit(1);
+	}
+	arg2 = strtod(argv[3], &error);
+	if (*error != '\0')
+	{
+		cout << "It's not number, please enter number" << endl;
+		exit(1);
+	}
+	z = *argv[2];
+	switch (argv[2][0])
+	{
+	case '+':
+		if (z == '+')
+			res = arg1 + arg2;
+	case '-':
+		if (z == '-')
+			res = arg1 - arg2;
+	case '/':
+		if (z == '/')
+			if (arg2 == 0.0)
+			{
+				cout << "Devidion by 0.\n";
+				exit(0);
+			}
+			else
+			{
+				res = arg1 / arg2;
+				break;
+			}
+	case '*':
+		if (z == '*')
+			res = arg1 * arg2;
+		break;
+	default:
+		cout << "Wrong operator .\n";
+			exit(2);
+	}
+	cout << "Answer is " << res << endl;
+	return 0;
+}
